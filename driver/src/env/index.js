ffe73515444b4124abb3cfc632dfbb7c070c6b02
Ankit Kumar <ankit@sugoilabs.com>
2023-04-30 20:08:08 +0530

    final commit

--- driver/src/env/index.js
+++ driver/src/env/index.js
@@ -6,5 +6,5 @@ export const APP_URL_ENV = 'http://10.0.2.2:3000'
 export const BASE_URL_ENV = 'https://backend-production-2c46.up.railway.app'
 export const API_URL_ENV = 'https://backend-production-2c46.up.railway.app'
 
-export const GOOGLE_MAP_API_KEY_ENV = 'AIzaSyAODVzhnS0ZaDrc_jx6uTRLezLFNycelHc'
+export const GOOGLE_MAP_API_KEY_ENV = 'RAAHI_GOOGLE_API_KEY'
 export const API_BASE = '/api/v1'
--- passenger/src/services/config.js
+++ passenger/src/services/config.js
@@ -1,4 +1,4 @@
-export const GOOGLE_MAP_API_KEY = 'AIzaSyAODVzhnS0ZaDrc_jx6uTRLezLFNycelHc'
+export const GOOGLE_MAP_API_KEY = 'RAAHI_GOOGLE_API_KEY'
 
 // export const BASE_URL = 'http://10.0.2.2:8080/api/v1/customer'
 export const BASE_URL = 'https://backend-production-2c46.up.railway.app/api/v1/customer'
